32589393e7c5af20f554c58b005bfce80de504ed
Long Quach <long.quach@hdwebsoft.dev>
2024-07-31 18:04:55 +0700

    update create Smart Trigger

--- src/main/java/HomeMeta/PageObjects/CreateCampaigns.java
+++ src/main/java/HomeMeta/PageObjects/CreateCampaigns.java
@@ -1,5 +1,8 @@
 package HomeMeta.PageObjects;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -42,6 +45,21 @@ public class CreateCampaigns extends AbstractComponent {
 
 	@FindBy(xpath = "//div[contains(text(),'Email')]")
 	WebElement typeEmail;
+	@FindBy(xpath = "//div[contains(text(),'Mail Campaign')]")
+	WebElement typeMailCampaign;
+	@FindBy(xpath = "//div[contains(text(),'Text')]")
+	WebElement typeText;
+	@FindBy(xpath = "//div[contains(text(),'Agent Notification')]")
+	WebElement agentNoti;
+	By lbl = By.xpath("//p[normalize-space()='Message Editor']");
+	@FindBy(xpath = "//span[normalize-space()='agent_name']")
+	WebElement agentName;
+	@FindBy(xpath = "//span[normalize-space()='agent_email']")
+	WebElement agentEmail;
+	@FindBy(xpath = "//span[normalize-space()='full_name']")
+	WebElement fullName;
+	@FindBy(xpath = "(//span[contains(text(),'Save')])[2]")
+	WebElement btnSave;
 
 	@FindBy(xpath = "//span[normalize-space()='Save Template']")
 	WebElement saveTemplate;
@@ -53,6 +71,13 @@ public class CreateCampaigns extends AbstractComponent {
 	@FindBy(xpath = "//p[@title='Drip Campaign']")
 	WebElement templateName;
 	By templateName1 = By.xpath("//p[@title='Drip Campaign']");
+	By lblTemplate = By.xpath("//p[@class='personal-information']");
+
+	@FindBy(xpath = "//span[@title='Any Type']")
+	WebElement clickType;
+	@FindBy(xpath = "//div[contains(text(),'Postcards only')]")
+	WebElement chooseType;
+	By typePostcard = By.xpath("//div[@class='postcard-img']");
 
 	@FindBy(xpath = "//span[normalize-space()='View Orders']")
 	WebElement btnViewOrder;
@@ -64,6 +89,8 @@ public class CreateCampaigns extends AbstractComponent {
 
 	@FindBy(xpath = "//button[@class='ant-btn ant-btn-default btn-save drip-field']")
 	WebElement saveAction;
+	By saveAction1 = By.xpath("//button[@class='ant-btn ant-btn-default btn-save drip-field']");
+
 	@FindBy(xpath = "//span[normalize-space()='SAVE DRIP CAMPAIGN']")
 	WebElement saveDripCampaign;
 	@FindBy(xpath = "//button[@aria-label='Accept cookies']")
@@ -80,8 +107,32 @@ public class CreateCampaigns extends AbstractComponent {
 	@FindBy(xpath = "//div[@name='response_type']//div[@class='ant-select-selector']")
 	WebElement responeType2;
 
-	@FindBy(xpath = "//span[@aria-label='edit']//*[name()='svg']")
+	@FindBy(xpath = "//div[@class='action-btn-group']//span[@aria-label='edit']")
 	WebElement editname;
+	By editname1 = By.xpath("//h2[normalize-space()='Add Drip For Smart Trigger']");
+	By title = By.xpath("//span[@class='title-content']");
+
+	@FindBy(xpath = "(//*[name()='svg'][@stroke='currentColor'])[6]")
+	WebElement duplicated;
+	@FindBy(xpath = "(//*[name()='svg'])[12]")
+	WebElement deleted;
+	@FindBy(xpath = "(//*[name()='path'])[15]")
+	WebElement clone;
+
+	@FindBy(xpath = "//span[normalize-space()='Confirm']")
+	WebElement confirm;
+	By confirm1 = By.xpath("//span[normalize-space()='Confirm']");
+
+	@FindBy(xpath = "//span[normalize-space()='Go To Drip Campaigns']")
+	WebElement goDripCampaign;
+	By goDripCampaign1 = By.xpath("//span[normalize-space()='Go To Drip Campaigns']");
+
+	@FindBy(xpath = "//h2[normalize-space()='Rename Drip Type Mail Campaign Auto (Copy)']")
+	WebElement campaignDuplicated;
+	By campaignDuplicated1 = By.xpath("//h2[normalize-space()='Rename Drip Type Mail Campaign Auto (Copy)']");
+	By nameDripCampaign = By.xpath("//h2[normalize-space()]");
+
+	// h2[normalize-space()]
 	@FindBy(xpath = "//input[@placeholder='Enter New Name']")
 	WebElement nameCampaign;
 	By nameCampaign1 = By.xpath("//input[@placeholder='Enter New Name']");
@@ -94,24 +145,20 @@ public class CreateCampaigns extends AbstractComponent {
 	@FindBy(xpath = "//h2[normalize-space()]")
 	private List<WebElement> smartTriggersTitles;
 
-	public void createDripsSmartStriggers(String smartStriggerName) throws InterruptedException {
+	public void createDripsSmartStriggersTypeEmail(String smartStriggerName) throws InterruptedException {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement Element = driver.findElement(By.xpath("//a[normalize-space()='Drip Campaigns']"));
-		js.executeScript("arguments[0].scrollIntoView();", Element);
-		menuDripCampaign.click();
+		waitForElementToApear(title);
+		goToDripSmartTrigger();
 		addNewDrip.click();
-		Thread.sleep(3000);
-
+		waitForElementToApear(editname1);
 		editname.click();
 		waitForElementToApear(nameCampaign1);
 		Actions a = new Actions(driver);
 		a.moveToElement(nameCampaign).doubleClick().click().sendKeys(Keys.BACK_SPACE).perform();
 		Thread.sleep(1000);
-
 		nameCampaign.sendKeys(smartStriggerName);
 		saveDripname.click();
 		waitForElementToApear(addDrip1);
-
 		addDrip.click();
 		waitForElementToApear(responeType1);
 		responeType.click();
@@ -119,14 +166,107 @@ public class CreateCampaigns extends AbstractComponent {
 		waitForElementToApear(templateName1);
 		templateName.click();
 		Thread.sleep(1500);
-
-		WebElement Element1 = driver.findElement(By.xpath("//span[normalize-space()='Save Template']"));
+		WebElement Element1 = saveTemplate;
 		js.executeScript("arguments[0].scrollIntoView();", Element1);
 		saveTemplate.click();
 		saveAction.click();
-		WebElement Element2 = driver.findElement(By.xpath("//span[normalize-space()='SAVE DRIP CAMPAIGN']"));
+		WebElement Element2 = saveDripCampaign;
 		js.executeScript("arguments[0].scrollIntoView();", Element2);
 		acceptCookies.click();
+		saveDripCampaign.click();
+		Thread.sleep(1000);
+	}
+
+	public void createDripsSmartStriggersTypeText(String smartStriggerName) throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		addNewDrip.click();
+		waitForElementToApear(editname1);
+		editname.click();
+		waitForElementToApear(nameCampaign1);
+		Actions a = new Actions(driver);
+		a.moveToElement(nameCampaign).doubleClick().click().sendKeys(Keys.BACK_SPACE).perform();
+		Thread.sleep(1000);
+		nameCampaign.sendKeys(smartStriggerName);
+		saveDripname.click();
+		waitForElementToApear(addDrip1);
+		addDrip.click();
+		waitForElementToApear(responeType1);
+		responeType.click();
+		typeText.click();
+		waitForElementToApear(lbl);
+		agentName.click();
+		Thread.sleep(500);
+		agentEmail.click();
+		Thread.sleep(500);
+		fullName.click();
+		btnSave.click();
+		Thread.sleep(2000);
+		waitForElementToApear(saveAction1);
+		saveAction.click();
+		WebElement Element2 = saveDripCampaign;
+		js.executeScript("arguments[0].scrollIntoView();", Element2);
+		saveDripCampaign.click();
+		Thread.sleep(1000);
+	}
+
+	public void createDripsAgentNoti(String smartStriggerName) throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		addNewDrip.click();
+		waitForElementToApear(editname1);
+		editname.click();
+		waitForElementToApear(nameCampaign1);
+		Actions a = new Actions(driver);
+		a.moveToElement(nameCampaign).doubleClick().click().sendKeys(Keys.BACK_SPACE).perform();
+		Thread.sleep(1000);
+		nameCampaign.sendKeys(smartStriggerName);
+		saveDripname.click();
+		waitForElementToApear(addDrip1);
+		addDrip.click();
+		waitForElementToApear(responeType1);
+		responeType.click();
+		agentNoti.click();
+		waitForElementToApear(lbl);
+		agentEmail.click();
+		Thread.sleep(500);
+		fullName.click();
+		btnSave.click();
+		Thread.sleep(2000);
+		waitForElementToApear(saveAction1);
+		saveAction.click();
+		WebElement Element2 = saveDripCampaign;
+		js.executeScript("arguments[0].scrollIntoView();", Element2);
+		saveDripCampaign.click();
+		Thread.sleep(1000);
+	}
+
+	public void createDripsSmartStriggersTypeMailCampaign(String smartStriggerName) throws InterruptedException {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		addNewDrip.click();
+		waitForElementToApear(editname1);
+		editname.click();
+		waitForElementToApear(nameCampaign1);
+		Actions a = new Actions(driver);
+		a.moveToElement(nameCampaign).doubleClick().click().sendKeys(Keys.BACK_SPACE).perform();
+		Thread.sleep(1000);
+		nameCampaign.sendKeys(smartStriggerName);
+		saveDripname.click();
+		waitForElementToApear(addDrip1);
+		addDrip.click();
+		waitForElementToApear(responeType1);
+		responeType.click();
+		typeMailCampaign.click();
+		waitForElementToApear(lblTemplate);
+		clickType.click();
+		chooseType.click();
+		Thread.sleep(6000);
+		WebElement Element1 = saveTemplate;
+		js.executeScript("arguments[0].scrollIntoView();", Element1);
+		saveTemplate.click();
+		saveAction.click();
+		WebElement Element2 = saveDripCampaign;
+		js.executeScript("arguments[0].scrollIntoView();", Element2);
 		saveDripCampaign.click();
 		Thread.sleep(1000);
 	}
@@ -135,14 +275,11 @@ public class CreateCampaigns extends AbstractComponent {
 
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		waitForElementToApear(menuDripCampaign1);
-
 		goToDripCampaign();
-
 		btnaddNewDrip.click();
 		Thread.sleep(3000);
 		editname.click();
 		waitForElementToApear(nameCampaign1);
-
 		Actions a = new Actions(driver);
 		a.moveToElement(nameCampaign).doubleClick().click().sendKeys(Keys.BACK_SPACE).perform();
 		Thread.sleep(1000);
@@ -155,16 +292,54 @@ public class CreateCampaigns extends AbstractComponent {
 		waitForElementToApear(templateName1);
 		templateName.click();
 		Thread.sleep(1500);
-
-		WebElement Element1 = driver.findElement(By.xpath("//span[normalize-space()='Save Template']"));
+		WebElement Element1 = saveTemplate;
 		js.executeScript("arguments[0].scrollIntoView();", Element1);
 		saveTemplate.click();
 		saveAction.click();
-		WebElement Element2 = driver.findElement(By.xpath("//span[normalize-space()='SAVE DRIP CAMPAIGN']"));
+		WebElement Element2 = saveDripCampaign;
 		js.executeScript("arguments[0].scrollIntoView();", Element2);
-		acceptCookies.click();
 		saveDripCampaign.click();
+	}
 
+	public void renameDripsmartTrigger(String renameDripCampaign) throws InterruptedException {
+		editname.click();
+		waitForElementToApear(nameCampaign1);
+		Actions a = new Actions(driver);
+		a.moveToElement(nameCampaign).doubleClick().click().sendKeys(Keys.BACK_SPACE).perform();
+		Thread.sleep(1000);
+		nameCampaign.sendKeys(renameDripCampaign);
+		saveDripname.click();
+	}
+
+	public void duplicatedDripsmartTrigger() throws InterruptedException {
+		String nameDuplicatedExpected = "Rename Drip Type Mail Campaign Auto (Copy)";
+		duplicated.click();
+		waitForElementToApear(confirm1);
+		confirm.click();
+		waitForElementToApear(campaignDuplicated1);
+		String nameDuplicated = campaignDuplicated.getText();
+		assertEquals(nameDuplicated, nameDuplicatedExpected);
+		System.out.println("Verify duplicated Drip SmartTrigger success");
+	}
+
+	public void deledtedDripsmartTrigger() throws InterruptedException {
+		deleted.click();
+		waitForElementToApear(confirm1);
+		confirm.click();
+		waitForElementToApear(nameDripCampaign);
+		assertFalse(false, campaignDuplicated.getText());
+		System.out.println("Deleted Drip SmartTrigger success");
+	}
+
+	public void cloneDripsmartTrigger() throws InterruptedException {
+		clone.click();
+		waitForElementToApear(confirm1);
+		confirm.click();
+		waitForElementToApear(goDripCampaign1);
+		goDripCampaign.click();
+		waitForElementToApear(nameDripCampaign);
+		//
+		System.out.println("Clone Drip SmartTrigger success");
 	}
 
 	public void goTo() {
@@ -174,6 +349,11 @@ public class CreateCampaigns extends AbstractComponent {
 
 	public void goToDripCampaign() {
 		driver.get("https://staging-realestate.homemeta.io/drip-campaigns/?tab=DRIP_CAMPAIGN");
+
+	}
+
+	public void goToDripSmartTrigger() {
+		driver.get("https://staging-realestate.homemeta.io/drip-campaigns");
 
 	}
 
@@ -190,6 +370,12 @@ public class CreateCampaigns extends AbstractComponent {
 	}
 
 	public Boolean VerifySmartTriggersDisplay(String smartStriggerName) {
+		Boolean match = smartTriggersTitles.stream()
+				.anyMatch(triggersName -> triggersName.getText().equalsIgnoreCase(smartStriggerName));
+		return match;
+	}
+
+	public Boolean VerifyRenameSmartTrigger(String smartStriggerName) {
 		Boolean match = smartTriggersTitles.stream()
 				.anyMatch(triggersName -> triggersName.getText().equalsIgnoreCase(smartStriggerName));
 		return match;
--- src/main/java/HomeMeta/PageObjects/CreateDripsSmartStriggers.java
+++ src/main/java/HomeMeta/PageObjects/CreateDripsSmartStriggers.java
@@ -12,16 +12,56 @@ import HomeMeta.TestComponents.BaseTest;
 
 public class CreateDripsSmartStriggers extends BaseTest {
 	@Test(dataProvider = "getData")
-	public void CreateDripSmartStriggers(HashMap<String, String> input) throws InterruptedException {
-		String smartStriggerName = "Drip Auto";
+	public void CreateDripSmartStriggersTypeEmail(HashMap<String, String> input) throws InterruptedException {
+		String smartStriggerName = "Drip Type Email Auto";
+		String smartStriggerTypeText = "Drip Type Text Auto";
+		String smartStriggerAgentNoti = "Drip Type Agent Noti Auto";
+		String smartStriggerTypeMailCampaign = "Drip Type Mail Campaign Auto";
+		String renameDripCampaign = "Rename Drip Type Mail Campaign Auto";
 		landingPage.login(input.get("email"), input.get("password"));
-
 		CreateCampaigns createDripCampaigns = new CreateCampaigns(driver);
-		createDripCampaigns.createDripsSmartStriggers(smartStriggerName);
-
+		createDripCampaigns.createDripsSmartStriggersTypeEmail(smartStriggerName);
+		Thread.sleep(2000);
 		Boolean match = createDripCampaigns.VerifySmartTriggersDisplay(smartStriggerName);
 		Assert.assertTrue(match);
-		System.out.println("Add Drips for Smart Triggers Success");
+		System.out.println("Add Drips for Smart Triggers Type Email Success");
+		Thread.sleep(3000);
+
+		createDripCampaigns.createDripsSmartStriggersTypeText(smartStriggerTypeText);
+		Thread.sleep(2000);
+		Boolean match1 = createDripCampaigns.VerifySmartTriggersDisplay(smartStriggerName);
+		Assert.assertTrue(match1);
+		System.out.println("Add Drips for Smart Triggers Type Text Success");
+		Thread.sleep(3000);
+
+		createDripCampaigns.createDripsAgentNoti(smartStriggerAgentNoti);
+		Thread.sleep(2000);
+		Boolean match2 = createDripCampaigns.VerifySmartTriggersDisplay(smartStriggerName);
+		Assert.assertTrue(match2);
+		System.out.println("Add Drips for Smart Triggers Agent Noti Success");
+		Thread.sleep(3000);
+
+		createDripCampaigns.createDripsSmartStriggersTypeMailCampaign(smartStriggerTypeMailCampaign);
+		Thread.sleep(2000);
+		Boolean match3 = createDripCampaigns.VerifySmartTriggersDisplay(smartStriggerName);
+		Assert.assertTrue(match3);
+		System.out.println("Add Drips for Smart Triggers Type Mail Campaign Success");
+		Thread.sleep(3000);
+
+		createDripCampaigns.renameDripsmartTrigger(renameDripCampaign);
+		Boolean match4 = createDripCampaigns.VerifySmartTriggersDisplay(smartStriggerName);
+		Assert.assertTrue(match4);
+		System.out.println("Rename Smart Triggers Type Mail Campaign Success");
+		Thread.sleep(3000);
+
+		createDripCampaigns.duplicatedDripsmartTrigger();
+		Thread.sleep(3000);
+
+		createDripCampaigns.deledtedDripsmartTrigger();
+		Thread.sleep(3000);
+
+		createDripCampaigns.cloneDripsmartTrigger();
+
 	}
 
 	@DataProvider
